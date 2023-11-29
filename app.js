7a92d004bbd7fc84de6a1572e110846f38e536d6
mmcurry17 <mmcurry17@gmail.com>
2023-11-28 16:04:38 -0800

    working commit

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("test");
+const apiKey = "fc8420ad05d66ece97da69fdbe398813";
+const appId = "3a5c3cda";
+
+$.ajax({
+  method: "POST",
+  url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
+  headers: {
+    "Content-Type": "application/json",
+    "x-app-id": appId,
+    "x-app-key": apiKey,
+  },
+  data: JSON.stringify({
+    query: "gold fish",
+  }),
+  success: (data) => {
+    console.log(data);
+  },
+  error: (error) => {
+    console.error(`Error: ${error}`);
+  },
+});
